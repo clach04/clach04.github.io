8e020d4e1cdf19bf6c1506110bf5d5ae4f1e7166
clach04 <Chris.Clark@actian.com>
2016-07-01 18:17:36 -0700

    Skipstone with emulator support config for pebble

---
+++ pebble/skipstone/script.js
@@ -0,0 +1,89 @@
+$(document).bind('pageinit', function() {
+
+	var players = [];
+	var panels = 0;
+
+	var data = /data=([^&|^\/]*)/.exec(location.search);
+	if (data && data[1]) {
+		try {
+			var d = JSON.parse(atob(decodeURIComponent(data[1])));
+			players = d.players;
+		} catch(err) {
+
+		}
+	}
+
+	$.each(players, function(index, player) {
+		var newPanel = $('.panel-template-' + player.player).first().clone();
+		addNewPanel(player.title, newPanel);
+		$.each(player, function(index, value) {
+			newPanel.find('#'+index).val(value);
+		});
+	});
+
+	function addNewPanel(title, newPanel) {
+		newPanel.find('.accordion-toggle').attr('href', '#panel' + (++panels));
+		newPanel.find('.panel-collapse').attr('id', 'panel' + panels);
+		newPanel.find('.panel-title').text(title);
+		$('#accordion').append(newPanel.fadeIn());
+		newPanel.find('#title').change(function() {
+			newPanel.find('.panel-title').text($(this).val());
+		});
+		newPanel.find('.btn-delete').click(function() {
+			newPanel.remove();
+		});
+		newPanel.find('.panel-heading').on('swipe', function() {
+			if(newPanel.find('.btn-delete').is(':hidden')) { newPanel.find('.btn-delete').show(); } else { newPanel.find('.btn-delete').hide(); }
+		});
+	}
+
+	$('.btn-add-plex').on('click', function() {
+		addNewPanel('New Plex Remote', $('.panel-template-0').first().clone());
+	});
+
+	$('.btn-add-vlc').on('click', function() {
+		addNewPanel('New VLC Remote', $('.panel-template-1').first().clone());
+	});
+
+	$('.btn-add-xbmc').on('click', function() {
+		addNewPanel('New XBMC Remote', $('.panel-template-2').first().clone());
+	});
+
+	$('.btn-add-wdtv').on('click', function() {
+		addNewPanel('New WDTV Remote', $('.panel-template-3').first().clone());
+	});
+
+	function getQueryParam(variable, defaultValue) {
+		var query = location.search.substring(1);
+		var vars = query.split('&');
+		for (var i = 0; i < vars.length; i++) {
+			var pair = vars[i].split('=');
+			if (pair[0] === variable) {
+				return decodeURIComponent(pair[1]);
+			}
+		}
+		return defaultValue || false;
+	}
+	$('.btn-save').on('click', function() {
+		var players = $.map($('.panel'), function(e) {
+			if ($(e).find('.panel-collapse').attr('id').indexOf('template') != -1)
+				return;
+			var player = {};
+			$(e).find('input').map(function() {
+				player[this.id] = $(this).val();
+			});
+			return player;
+		});
+		var ret = {
+			players: players
+		};
+		// Set the return URL depending on the runtime environment (i.e. Pebble or emulator)
+		var return_to = getQueryParam('return_to', 'pebblejs://close#');
+		document.location = return_to + encodeURIComponent(btoa(JSON.stringify(ret)));
+	});
+
+	$('.panel-container').sortable({
+		handle: '.handle',
+		axis: 'y'
+	});
+});
